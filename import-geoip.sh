10fc9aedd4c961c26647aac90fadf5af219b4fa0
Crzidea <i@crzidea.com>
2024-08-21 20:20:59 +0800

    Add geoip database scripts

---
+++ import-geoip.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# 1. Download file to tmp folder from one of the following links
+# https://www.maxmind.com/en/home
+# https://github.com/Loyalsoldier/geoip?tab=readme-ov-file
+# 2. Create new sqlite database and import from .csv
+mkdir -p tmp
+cd tmp
+database_filename=geolite2-contry.db
+geolite2_country_dirname=`ls -d GeoLite2-Country-CSV_*`
+database_version=`echo $geolite2_country_dirname | sed 's/GeoLite2-Country-CSV_//'`
+merged_table_name="merged_ipv4_data"
+rm -f $database_filename
+sqlite3 $database_filename ".mode csv" ".import $geolite2_country_dirname/GeoLite2-Country-Blocks-IPv4.csv blocks_ipv4"
+sqlite3 $database_filename ".mode csv" ".import $geolite2_country_dirname/GeoLite2-Country-Locations-en.csv locations"
+sed "s/merged_ipv4_data/$merged_table_name/g" ../merge.sql | sqlite3 $database_filename
+sqlite3 $database_filename ".schema $merged_table_name" ".dump $merged_table_name --data-only" >dump.sql
+# Login
+# npx wrangler login
+# Upload the dump file to cloudflare
+database="geolite2_contry_$database_version"
+npx wrangler d1 create $database
+npx wrangler d1 execute $database --remote --file=dump.sql
---
+++ merge.sql
@@ -0,0 +1,38 @@
+CREATE TABLE merged_ipv4_data (
+    network_start INTEGER,
+    network_end INTEGER,
+    country_iso_code TEXT,
+    network TEXT
+);
+CREATE INDEX IF NOT EXISTS idx_network_range ON merged_ipv4_data (network_start, network_end);
+
+INSERT INTO merged_ipv4_data (network_start, network_end, country_iso_code, network)
+SELECT
+    -- Calculate network_start
+    (
+        (
+            (CAST(SUBSTR(network, 1, INSTR(network, '.') - 1) AS INTEGER) * 256 * 256 * 256) +
+            (CAST(SUBSTR(SUBSTR(network, INSTR(network, '.') + 1), 1, INSTR(SUBSTR(network, INSTR(network, '.') + 1), '.') - 1) AS INTEGER) * 256 * 256) +
+            (CAST(SUBSTR(SUBSTR(SUBSTR(network, INSTR(network, '.') + 1), INSTR(SUBSTR(network, INSTR(network, '.') + 1), '.') + 1), 1, INSTR(SUBSTR(SUBSTR(network, INSTR(network, '.') + 1), INSTR(SUBSTR(network, INSTR(network, '.') + 1), '.') + 1), '.') - 1) AS INTEGER) * 256) +
+            CAST(SUBSTR(SUBSTR(SUBSTR(SUBSTR(network, INSTR(network, '.') + 1), INSTR(SUBSTR(network, INSTR(network, '.') + 1), '.') + 1), INSTR(SUBSTR(SUBSTR(network, INSTR(network, '.') + 1), INSTR(SUBSTR(network, INSTR(network, '.') + 1), '.') + 1), '.') + 1), 1, INSTR(SUBSTR(SUBSTR(SUBSTR(network, INSTR(network, '.') + 1), INSTR(SUBSTR(network, INSTR(network, '.') + 1), '.') + 1), INSTR(SUBSTR(SUBSTR(network, INSTR(network, '.') + 1), INSTR(SUBSTR(network, INSTR(network, '.') + 1), '.') + 1), '.') + 1), '/') - 1) AS INTEGER)
+        ) & (4294967295 << (32 - CAST(SUBSTR(network, INSTR(network, '/') + 1) AS INTEGER)))
+    ) AS network_start,
+
+    -- Calculate network_end
+    (
+        (
+            (CAST(SUBSTR(network, 1, INSTR(network, '.') - 1) AS INTEGER) * 256 * 256 * 256) +
+            (CAST(SUBSTR(SUBSTR(network, INSTR(network, '.') + 1), 1, INSTR(SUBSTR(network, INSTR(network, '.') + 1), '.') - 1) AS INTEGER) * 256 * 256) +
+            (CAST(SUBSTR(SUBSTR(SUBSTR(network, INSTR(network, '.') + 1), INSTR(SUBSTR(network, INSTR(network, '.') + 1), '.') + 1), 1, INSTR(SUBSTR(SUBSTR(network, INSTR(network, '.') + 1), INSTR(SUBSTR(network, INSTR(network, '.') + 1), '.') + 1), '.') - 1) AS INTEGER) * 256) +
+            CAST(SUBSTR(SUBSTR(SUBSTR(SUBSTR(network, INSTR(network, '.') + 1), INSTR(SUBSTR(network, INSTR(network, '.') + 1), '.') + 1), INSTR(SUBSTR(SUBSTR(network, INSTR(network, '.') + 1), INSTR(SUBSTR(network, INSTR(network, '.') + 1), '.') + 1), '.') + 1), 1, INSTR(SUBSTR(SUBSTR(SUBSTR(network, INSTR(network, '.') + 1), INSTR(SUBSTR(network, INSTR(network, '.') + 1), '.') + 1), INSTR(SUBSTR(SUBSTR(network, INSTR(network, '.') + 1), INSTR(SUBSTR(network, INSTR(network, '.') + 1), '.') + 1), '.') + 1), '/') - 1) AS INTEGER)
+        ) | (4294967295 >> CAST(SUBSTR(network, INSTR(network, '/') + 1) AS INTEGER))
+    ) AS network_end,
+
+    locations.country_iso_code,
+    blocks_ipv4.network
+FROM
+    blocks_ipv4
+JOIN
+    locations
+ON
+    blocks_ipv4.registered_country_geoname_id = locations.geoname_id;

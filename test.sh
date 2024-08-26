1784336775e133a6f7fb972ca9cad0dcd2d9fc46
Crzidea <i@crzidea.com>
2024-08-26 15:49:37 +0800

    Use www.aliyun.com instead of www.qq.com because of CDN quality

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec curl --doh-insecure --doh-url "https://localhost:8787/223.70.235.2/dns-query" www.qq.com -I
+exec curl --doh-insecure --doh-url "https://localhost:8787/223.70.235.2/dns-query" "https://www.aliyun.com" -I

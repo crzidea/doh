e9365c0f60f7da0c60445a97c50cc157a280f46b
Crzidea <i@crzidea.com>
2024-09-03 16:56:31 +0800

    Add data location environment

--- import-geoip.sh
+++ import-geoip.sh
@@ -8,6 +8,8 @@ cd tmp
 database_filename=geolite2-contry.db
 geolite2_country_dirname=`ls -d GeoLite2-Country-CSV_*`
 database_version=`echo $geolite2_country_dirname | sed 's/GeoLite2-Country-CSV_//'`
+# https://developers.cloudflare.com/d1/configuration/data-location/
+database_location="weur"
 merged_table_name="merged_ipv4_data"
 rm -f $database_filename
 sqlite3 $database_filename ".mode csv" ".import $geolite2_country_dirname/GeoLite2-Country-Blocks-IPv4.csv blocks_ipv4"
@@ -17,6 +19,6 @@ sqlite3 $database_filename ".schema $merged_table_name" ".dump $merged_table_nam
 # Login
 # npx wrangler login
 # Upload the dump file to cloudflare
-database="geolite2_contry_$database_version"
-npx wrangler d1 create $database
+database="geolite2_contry_${database_version}_${database_location}"
+npx wrangler d1 create $database --location=$database_location
 npx wrangler d1 execute $database --remote --file=dump.sql
